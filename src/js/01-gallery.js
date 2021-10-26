671b27ed5e3388f8c3ddea854156ad9f7d14115c
Vitalii-Krat <kratvetal@ukr.net>
2021-10-26 21:24:09 +0300

    add-completed-tasks

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -2,4 +2,45 @@
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 console.log(galleryItems);
+
+const galleryDiv = document.querySelector('.gallery')
+
+
+
+// 1. Создание и рендер разметки по массиву данных galleryItems 
+//и предоставленному шаблону элемента галереи.
+
+const galleryMarkup = createPhotoMarkup(galleryItems);
+
+galleryDiv.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createPhotoMarkup(elements) {
+  return elements
+    .map(({ preview, original, description }) => {
+      return `
+<a class="gallery__item" 
+href="${original}">
+  <img
+  class="gallery__image"
+   data-source="${original}"
+  src="${preview}"
+   alt="${description}"
+   title ="${description}"/>
+</a>
+    `;
+    })
+    .join('');
+}
+
+// 2. Инициализация библиотеки 
+// Добавляет отображение подписей к изображениям из атрибута alt.
+// Пусть подпись будет снизу и появляется
+// через 250 миллисекунд после открытия изображения.
+
+let gallery = new SimpleLightbox('.gallery a', ({captionDelay: '250'}));
+gallery.on('show.simplelightbox', function () {
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
 
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+function onCatchTimePlay() {
+  player.getCurrentTime().then(function (seconds) {
+      localStorage.setItem('videoplayer-current-time', seconds);
+  })};
+
+player.on('timeupdate', throttle(onCatchTimePlay, 1000));
+const stopTime = localStorage.getItem('videoplayer-current-time');
+player.setCurrentTime(stopTime);
